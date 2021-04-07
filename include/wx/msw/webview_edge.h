5fc6c63c334141ab79aebdae89fe80ef0ac88d1d
Tobias Taschner <TcT2k@users.noreply.github.com>
2021-04-07 22:21:04 +0200

    Add wxWebView::RunScriptAsync()

--- include/wx/msw/webview_edge.h
+++ include/wx/msw/webview_edge.h
@@ -88,7 +88,8 @@ public:
 
     virtual bool SetUserAgent(const wxString& userAgent) wxOVERRIDE;
 
-    virtual bool RunScript(const wxString& javascript, wxString* output = NULL) const wxOVERRIDE;
+    virtual void RunScriptAsync(const wxString& javascript, wxWindowID id = -1,
+        void* clientData = NULL) const wxOVERRIDE;
     virtual bool AddScriptMessageHandler(const wxString& name) wxOVERRIDE;
     virtual bool RemoveScriptMessageHandler(const wxString& name) wxOVERRIDE;
     virtual bool AddUserScript(const wxString& javascript,
